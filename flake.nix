ca631728b60f7de95bd78f7f5184a482bfe467ef
Liam Bunch <liam@liambunch.com>
2026-06-28 17:20:54 +1000

    Add initial flake.nix configuration for NixOS

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Liam's NixOS Desktop";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          home-manager.nixosModules.home-manager
+          ./configuration.nix
+        ];
+      };
+    };
+}
