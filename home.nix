2f6b5b9ee72dd3d652eb02cb8494750a4133fafa
Liam Bunch <liam@liambunch.com>
2026-07-04 12:58:03 +1000

    Updated Git config

--- home.nix
+++ home.nix
@@ -22,11 +22,18 @@
 
   programs.git = {
     enable = true;
+
+    signing = {
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
+
     settings = {
       user = {
         name = "Liam Bunch";
         email = "liam@liambunch.com";
       };
+      gpg.format = "ssh";
       init.defaultBranch = "main";
       pull.rebase = true;
     };
